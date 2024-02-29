5b1ab24b78ead0163da922dab70a8281205162d6
ppktr <151742714+ppktr@users.noreply.github.com>
2024-02-29 16:28:20 +0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -11,28 +11,28 @@ let currentIndex = 0; // Initialize the index to 0
 router.use(requestIp.mw());
 
 router.get('/', async function(req, res, next) {
-    const ipAddress = requestIp; // Hardcoded IP address for testing purposes
+  const ipAddress = requestIp.getClientIp(req); // Get client's IP address
+  
+  try {
+      // Fetch additional data from the IP address using IP-API service
+      const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
+      const location = response.data;
+      console.log(location);
 
-    try {
-        // Fetch additional data from the IP address using IP-API service
-        const response = await axios.get(`http://ip-api.com/json/${ipAddress}`);
-        const location = response.data;
-        console.log(location);
+      // Get the referring domain from the request headers
+      const referringDomain = req.headers.referer || 'Direct visit'; // If referer is not present, set it to 'Direct visit'
 
-        // Get the referring domain from the request headers
-        const referringDomain = req.headers.referer || 'Direct visit'; // If referer is not present, set it to 'Direct visit'
+      // Determine which website to redirect based on IP address
+      const redirectWeb = groupWeb[currentIndex];
+      res.redirect(`http://${redirectWeb}`);
+      console.log(referringDomain)
 
-        // Determine which website to redirect based on IP address
-        const redirectWeb = groupWeb[currentIndex];
-        res.redirect(`http://${redirectWeb}`);
-        console.log(referringDomain)
-
-        // Increment currentIndex to rotate to the next website
-        currentIndex = (currentIndex + 1) % groupWeb.length;
-    } catch (error) {
-        console.error('Error fetching data from IP-API:', error);
-        res.status(500).send('Internal Server Error');
-    }
+      // Increment currentIndex to rotate to the next website
+      currentIndex = (currentIndex + 1) % groupWeb.length;
+  } catch (error) {
+      console.error('Error fetching data from IP-API:', error);
+      res.status(500).send('Internal Server Error');
+  }
 });
 
 module.exports = router;
